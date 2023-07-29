967e6bd03ca4125638741c2c149dcf4140059316
AlbShar <sharipov.albert1991@gmail.com>
2023-07-29 19:07:59 +0500

    update fileInput styles

--- src/components/CustomForm/CustomForm.tsx
+++ src/components/CustomForm/CustomForm.tsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { createFolder } from "../../api/createFolder";
 import { sendFilesToDisk } from "../../api/sendFilesToDisk";
 import { fetchUrl } from "../../api/fetchUrl";
-import "./customForm.css"
-
-const urlHasLoaded = (responses: any, files: any) => {
-  for (const response of responses) {
-    if (response.href) {
-      sendFilesToDisk(response.href, files);
-    } else {
-      throw new Error("запрос с ошибкой");
-    }
-  }
-};
+import "./customForm.css";
 
 const CustomForm = () => {
+  const [textFileInput, setTextFileInput] =
+    useState<string>("Файлы не выбраны");
+
+  const urlHasLoaded = (responses: any, files: any) => {
+    for (const response of responses) {
+      if (response.href) {
+        sendFilesToDisk(response.href, files);
+      } else {
+        throw new Error("запрос с ошибкой");
+      }
+    }
+  };
+
   return (
     <Formik
       initialValues={{ files: [], folderName: "" }}
@@ -27,14 +31,18 @@ const CustomForm = () => {
           .max(15, "Максимальное количество символов - 15")
           .required("Поле обязательное для заполнения"),
       })}
-      onSubmit={async ({ files, folderName }) => {
+      onSubmit={async ({ files, folderName }, {resetForm}) => {
         const nameFiles = files.map((file) => file["name"]);
         const response = await createFolder(folderName);
         console.log(response);
 
-        Promise.all(
-          nameFiles.map((nameFile) => fetchUrl(folderName, nameFile))
-        ).then((responses) => urlHasLoaded(responses, files));
+        Promise.all(nameFiles.map((nameFile) => fetchUrl(folderName, nameFile)))
+          .then((responses) => {
+            urlHasLoaded(responses, files) 
+            resetForm()
+            setTextFileInput('Файлы не выбраны')
+          })
+          
       }}
     >
       {(formik) => {
@@ -42,16 +50,52 @@ const CustomForm = () => {
           <Form method="post" encType="multipart/form-data">
             <section>
               <article>
-                <label htmlFor="folderName" className="label">Название папки:</label>
-                <Field type="text" id="folderName" name="folderName" className="input"/>
-                <ErrorMessage name="folderName" component="div" className="error"/>
+                <label htmlFor="folderName" className="label">
+                  Название папки:
+                </label>
+                <Field
+                  type="text"
+                  id="folderName"
+                  name="folderName"
+                  className="input"
+                />
+                <ErrorMessage
+                  name="folderName"
+                  component="div"
+                  className="error"
+                />
               </article>
               <article>
                 <label htmlFor="file" className="label">
                   Выберите файлы для загрузки (от 1 до 100):
                 </label>
                 <div>
-                  <input
+                  <div className="field__wrapper">
+                    <input
+                      type="file"
+                      name="files"
+                      id="field__file-2"
+                      className="field field__file"
+                      multiple
+                      onChange={(event: any) => {
+                        const files = event.target.files;
+                        if (files) {
+                          setTextFileInput(`Выбрано файлов: ${files.length}`);
+                          let myFiles = Array.from(files);
+                          formik.setFieldValue("files", myFiles);
+                        }
+                      }}
+                    />
+
+                    <label
+                      className="field__file-wrapper"
+                      htmlFor="field__file-2"
+                    >
+                      <div className="field__file-button">Выбрать</div>
+                      <div className="field__file-fake">{textFileInput}</div>
+                    </label>
+                  </div>
+                  {/* <input
                     type="file"
                     id="file"
                     name="files"
@@ -63,14 +107,16 @@ const CustomForm = () => {
                         formik.setFieldValue("files", myFiles);
                       }
                     }}
-                  />
+                  /> */}
                 </div>
-                <ErrorMessage name="files" component="div" className="error"/>
+                <ErrorMessage name="files" component="div" className="error" />
               </article>
             </section>
 
             <p>
-              <button className="button" type="submit">Загрузить</button>
+              <button className="button" type="submit">
+                Загрузить
+              </button>
             </p>
           </Form>
         );
